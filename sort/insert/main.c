5eb61f5641b780c21c04f4f50f7a4e397a6b3391
H-Y-B <2652266098@qq.com>
2019-09-15 16:13:50 +0800

    add insert_sort

---
+++ sort/insert/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//From small to large
+//insert sort
+void insert(int array[],int num) //0 1 2 3... num-1
+{
+    for(int i=1;i<num;i++)
+    {
+       int temp=array[i];//the element that need to insert
+
+       int j=i;
+       for(;j>0;j--)
+       {
+           if(array[j-1]>temp)  //´ÓºóÍùÇ°ÕÒ
+            array[j]=array[j-1];
+           else
+            break;
+       }
+       array[j]=temp;
+    }
+}
+
+int random(int n)//produce [0,n)
+{
+    return rand()%n;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int num=random(99);
+
+    int *array=(int*)calloc(num,sizeof(int));
+
+    for(int i=0;i<num;i++)
+    {
+        array[i]=random(100);
+    }
+
+    insert(array,num);
+
+    for(int i=0;i<num;i++)
+    {
+        printf("%d:   %d\n",i,array[i]);
+    }
+
+    return 0;
+}
