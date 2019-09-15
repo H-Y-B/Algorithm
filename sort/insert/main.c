1ca49c42ad66e4705bf0dbad4ae8989c4ff10f9d
H-Y-B <2652266098@qq.com>
2019-09-15 16:17:50 +0800

    .

--- sort/insert/main.c
+++ sort/insert/main.c
@@ -8,12 +8,12 @@ void insert(int array[],int num) //0 1 2 3... num-1
 {
     for(int i=1;i<num;i++)
     {
-       int temp=array[i];//the element that need to insert
+       int temp=array[i];//the element that needs to insert
 
        int j=i;
        for(;j>0;j--)
        {
-           if(array[j-1]>temp)  //´ÓºóÍùÇ°ÕÒ
+           if(array[j-1]>temp)  //compare from back to front numbers
             array[j]=array[j-1];
            else
             break;
