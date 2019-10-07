d79ee54920fab645e465a506637a165b3ea4e28f
H-Y-B <2652266098@qq.com>
2019-10-07 12:06:20 +0800

    代码分析

--- others/select_part/select_part/select.c
+++ others/select_part/select_part/select.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 
-
+/*这里每次都选择 array[left] 来part
+   可以通过改变 x 的选择方法，来改进算法。*/
 int part(int array[],int left,int right){
 
     int i=left+1,j=right;
@@ -28,25 +29,25 @@ int part(int array[],int left,int right){
     return j;
 }
 
-
-
+/* 选择：
+        借鉴的快速排序的思想，
+        先part，找出一个数的排名，
+        然后，比较
+        继续part 所找排名所找在的区域。*/
 int select_part(int array[],int num,int paiming){//paiming:  0、1、2......n-1
 
     int left=0,right=num-1;
 
     while(1)
     {
-        int mid=part(array, left, right);
-
+        int mid=part(array, left, right);//先找到array[left]的排名
+                                         //此时array[left]已经处在它所在排名的正确位置上。
         if(mid<paiming){
             left=mid+1;
         }else if(mid>paiming){
             right=mid-1;
         }else{
-            return array[mid];
+            return array[mid]; //一定会return
         }
     }
-
-
-
 }
